6d0c4e414720f66da729f85706a3aeebb9e12b37
Lethe <1040563191@qq.com>
2018-07-06 17:14:16 +0800

    创建根目录

---
+++ Assets/Scripts/PostEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PostEffect : PostEffectsBase {
+
+	//public Shader matCapShader;
+	public Material matCapMaterial;
+	public Material material{
+		get{
+			return matCapMaterial;
+		}
+	}
+	
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if(material != null){
+			Graphics.Blit(src,dest,material);
+		}else{
+			Graphics.Blit(src,dest);
+		}
+	}
+}
